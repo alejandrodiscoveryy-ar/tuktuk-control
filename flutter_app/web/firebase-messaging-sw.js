49b41fef801a75c4046134f7f5c5f7900fb73cca
Alejandro Cruz <alejandrodiscoveryy@gmail.com>
2026-09-04 15:55:41 -0400

    fix(web): avoid duplicate icon in push notifications

--- flutter_app/web/firebase-messaging-sw.js
+++ flutter_app/web/firebase-messaging-sw.js
@@ -20,7 +20,6 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(title || 'TukTuk Control', {
     body: body || '',
-    icon: 'icons/Icon-192.png',
     data: payload.data || {}
   });
 });
